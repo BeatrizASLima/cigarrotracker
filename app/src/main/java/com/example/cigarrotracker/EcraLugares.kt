9a3522e71f52258944f8630b09120192912d1340
beatriz <a044416@umaia.pt>
2026-01-07 19:27:49 +0000

    .

--- app/src/main/java/com/example/cigarrotracker/EcraLugares.kt
+++ app/src/main/java/com/example/cigarrotracker/EcraLugares.kt
@@ -24,6 +24,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.compose.ui.text.style.TextAlign
